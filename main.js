8e74157be3d0f1ffdb6d908154b9a74bf6e8cfe4
LiubovSchmit <liubou.schmit@gmail.com>
2021-04-29 18:21:01 +0200

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 const array = [777,23,12,43,32,65,82,21,0,99]
+
+//по возрастанию
+for (let i =0; i<array.length; i++) {
+    if(array[i] > array[i+1]){
+        let temp = array[i+1]
+        array[i+1 ] = array[i]
+        array[i] = temp
+    }
+}
+console.log(array)
